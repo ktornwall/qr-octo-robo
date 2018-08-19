49ee96164f52b1452869e390cfd1ccc71981101f
Kat Tornwall <ktornwall@gmail.com>
2018-08-19 13:00:02 -0400

    Add icon to header

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -4,6 +4,8 @@ import RadioButtonGroup, {
   IRadioButtonGroupOption
 } from "../components/RadioButtonGroup";
 
+import HeaderIcon from "./HeaderIcon.png";
+
 export interface IProps {
   barcodeType: BarcodeEncoding;
   onBarcodeTypeChange: (type: BarcodeEncoding) => any;
@@ -30,8 +32,11 @@ class Header extends React.Component<IProps> {
     const { barcodeType } = this.props;
 
     return (
-      <header className="navbar navbar-dark bg-dark  flex-shrink-0">
-        <span className="navbar-brand">QR Octo Robo</span>
+      <header className="navbar navbar-dark bg-dark  flex-shrink-0 align-items-center">
+        <span className="navbar-brand  d-flex align-items-center">
+          <img src={HeaderIcon} height={30} width={30} className="mr-3" />
+          QR Octo Robo
+        </span>
 
         <RadioButtonGroup
           activeClass="btn-light"
