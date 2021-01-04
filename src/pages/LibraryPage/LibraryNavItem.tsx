17dd217b731db6109c0ceb1e0f7071fbefe5e592
Kat Fairbanks <ktornwall@gmail.com>
2021-01-03 22:23:30 -0500

    Add CI via Github Actions
    Add sample tests for the library nav item component that will pass.

--- src/pages/LibraryPage/LibraryNavItem.tsx
+++ src/pages/LibraryPage/LibraryNavItem.tsx
@@ -1,9 +1,18 @@
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import Button from "../../components/Button";
-import { Barcode } from "../../types/Barcode";
+import { Barcode, BarcodeType } from "../../types/Barcode";
 
-type Props = {
+const barcodeTypeEl = (barcodeType: BarcodeType): React.ReactNode => {
+  switch (barcodeType) {
+    case "code128":
+      return <div className="text-xs text-blue-800 font-mono mr-4">128</div>;
+    case "qrcode":
+      return <div className="text-xs text-green-800 font-mono mr-4">QR</div>;
+  }
+};
+
+export type Props = {
   barcode: Barcode;
 
   onClick: () => void;
@@ -11,11 +20,21 @@ type Props = {
 };
 
 const LibraryNavItem: React.FunctionComponent<Props> = (props) => {
+  const { barcode, onClick, onDelete } = props;
+
   return (
-    <div className="px-6 py-2 hover:bg-gray-100 flex flex-row items-center cursor-pointer">
-      <div className="text-xs text-green-800 font-mono mr-4">QR</div>
-      <div className="flex-grow">Barcode 1</div>
-      <Button variant="red-minimal" icon={faTimes} />
+    <div
+      className="px-6 py-2 hover:bg-gray-100 flex flex-row items-center cursor-pointer"
+      onClick={onClick}
+    >
+      {barcodeTypeEl(barcode.barcodeType)}
+      <div className="flex-grow">{barcode.content}</div>
+      <Button
+        variant="red-minimal"
+        icon={faTimes}
+        data-testid="delete-barcode-btn"
+        onClick={onDelete}
+      />
     </div>
   );
 };
